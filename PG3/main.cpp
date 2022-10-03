5b089a82d43db2ce895ccd963015943f27f43898
REO MIYA <41591rionmiyazaki@gmail.com>
2022-10-03 10:34:42 +0900

    確認課題1-1完成

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+template <typename Type>
+Type Min(Type a, Type b)
+{
+	if (a <= b)
+	{
+		return static_cast<Type>(a);
+	}
+	return static_cast<Type>(b);
+}
+
+template<>
+char Min<char>(char a, char b)
+{
+	return printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+}
+
+int main(void) {
+	printf("%d\n", Min<int>(123, 456));
+	printf("%f\n", Min<float>(12.3f, 1.23f));
+	printf("%lf\n", Min<double>(123.123, 123.456));
+	printf("%c\n", Min<char>('7', '6'));
+}
