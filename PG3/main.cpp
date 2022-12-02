0fafabf79022d565ca6e3034c22379c7790a5b58
REO MIYA <41591rionmiyazaki@gmail.com>
2022-12-02 22:18:35 +0900

    STLとコンテナクラス

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,1 +1,14 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	// intŒ^”z—ñ a(—v‘f”:10)
+	// int a[10];
+	// intŒ^‚Ì‰Â•Ï’·”z—ñ b(—v‘f”:0)
+	vector<int> a;
+
+	system("pause");
+	return 0;
+}
