34aebedde8f5c52dcbd59a5eb844a7d0c72848f5
REO MIYA <41591rionmiyazaki@gmail.com>
2022-12-02 22:02:31 +0900

    ラムダ式とstd::function

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,1 +1,10 @@
 #include <stdio.h>
+#include <functional>
+
+int main(int argc, const char* argv[])
+{
+	std::function<int(int)>fx = [](int i) {return i + 1; };
+	//auto fx2 = [](int i) {return i + 1; };
+	printf("%d\n", fx(5));
+	return 0;
+}
