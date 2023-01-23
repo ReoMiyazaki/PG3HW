cde8e0713897012663ee904fc66e3b4282753090
REO MIYA <41591rionmiyazaki@gmail.com>
2023-01-23 10:36:17 +0900

    途中経過

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <functional>
+#include <Windows.h>
 
 int main(int argc, const char* argv[])
 {
-	std::function<int(int)>fx = [](int i) {return i + 1; };
-	//auto fx2 = [](int i) {return i + 1; };
-	printf("%d\n", fx(5));
+	srand(time(nullptr));
+	int select = 0;
+
+	// 半か丁を選んでもらう
+	/*printf("サイコロの出目は、半か丁か！\n");
+	printf("半なら1, 丁なら0を入力してください → ");
+	scanf("%d", &select);*/
+
+	std::function<int()> dice;
+
 	return 0;
 }
