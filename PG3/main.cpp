213d512bf2504bc0377f2a0800a35f3d9ec1b806
REO MIYA <41591rionmiyazaki@gmail.com>
2023-01-23 10:58:39 +0900

    修正

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -7,34 +7,34 @@ int main()
 {	// 山手線の駅名のリスト
 	std::list<const char*>list
 	{
-		"Tokyo",				// 東京
-		"Kanda",				// 神田
-		"Akihabara",			// 秋葉原
-		"Okachimachi",			// 御徒町
-		"Ueno",					// 上野
-		"Uguisudani",			// 鶯谷
-		"Nippori",				// 日暮里 (この後に1971開業の西日暮里<Nishi-Nippori>が入る)
-		"Tabata",				// 田端
-		"Komagome",				// 駒込
-		"Sugamo",				// 巣鴨
-		"Otsuka",				// 大塚
-		"Ikebukuro",			// 池袋
-		"Mejiro",				// 目白
-		"Takadanobaba",			// 高田馬場
-		"Shin-Okubo",			// 新大久保
-		"Shinjuku",				// 新宿
-		"Yoyogi",				// 代々木
-		"Harajuku",				// 原宿
-		"Shibuya",				// 渋谷
-		"Ebisu",				// 恵比寿
-		"Meguro",				// 目黒
-		"Gotanda",				// 五反田
-		"Osaki",				// 大崎
-		"Shinagawa",			// 品川 (この後に2020開業の高輪ゲートウェイ<Takanawa-Gateway>が入る)
-		"Tamachi",				// 田町
-		"Hamamatsucho",			// 浜松町
-		"Shimbashi",			// 新橋
-		"Yurakucho",			// 有楽町
+		"Tokyo",		// 東京
+		"Kanda",		// 神田
+		"Akihabara",	// 秋葉原
+		"Okachimachi",	// 御徒町
+		"Ueno",			// 上野
+		"Uguisudani",	// 鶯谷
+		"Nippori",		// 日暮里 (この後1971開業の西日暮里<Nishi-Nippori>)
+		"Tabata",		// 田端
+		"Komagome",		// 駒込
+		"Sugamo",		// 巣鴨
+		"Otsuka",		// 大塚
+		"Ikebukuro",	// 池袋
+		"Mejiro",		// 目白
+		"Takadanobaba",	// 高田馬場
+		"Shin-Okubo",	// 新大久保
+		"Shinjuku",		// 新宿
+		"Yoyogi",		// 代々木
+		"Harajuku",		// 原宿
+		"Shibuya",		// 渋谷
+		"Ebisu",		// 恵比寿
+		"Meguro",		// 目黒
+		"Gotanda",		// 五反田
+		"Osaki",		// 大崎
+		"Shinagawa",	// 品川 (この後2020開業の高輪ゲートウェイ<Takanawa-Gateway>)
+		"Tamachi",		// 田町
+		"Hamamatsucho",	// 浜松町
+		"Shimbashi",	// 新橋
+		"Yurakucho",	// 有楽町
 	};
 
 	printf("1970年\n");
