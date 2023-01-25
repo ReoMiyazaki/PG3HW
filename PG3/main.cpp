1d05015426d1c0649cac8d0ccd141cd19363fa5f
REO MIYA <41591rionmiyazaki@gmail.com>
2023-01-25 16:52:00 +0900

    ラムダ式完成

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,20 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <functional>
 #include <Windows.h>
+#include <functional>
+
+// コールバック関数
+void setTimeout(std::function<void()>Judgeunction, int second)
+{
+	// コールバック関数を呼び出す
+	Sleep(second * 1000);
+
+	Judgeunction();
+}
 
 int main(int argc, const char* argv[])
 {
 	srand(time(nullptr));
-	int select = 0;
+	int anser = 0;
+	int dice = 0;
+	dice = rand() % 6 + 1;
 
-	// 半か丁を選んでもらう
-	/*printf("サイコロの出目は、半か丁か！\n");
+	printf("サイコロの出目は、半か丁か\n");
 	printf("半なら1, 丁なら0を入力してください → ");
-	scanf("%d", &select);*/
+	scanf_s("%d", &anser);
 
-	std::function<int()> dice;
+	std::function<void()> Judgeunction = [&]()
+	{
+		if (anser == dice % 2)
+		{
+			printf("あなたの勝ち\n");
+		}
+		else
+		{
+			printf("あなたの負け\n");
+		}
+		printf("ダイスの値は%d\n", dice);
+	};
+
+	// 3秒待つ
+	setTimeout(Judgeunction, 3);
 
 	return 0;
 }
